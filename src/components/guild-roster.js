dc30275599179173bdd9088a6e6cc2c830b323fd
Nathan <macCormack@users.noreply.github.com>
2018-10-07 15:53:25 -0400

    Added comments & 2nd switch
    Commented code and added a second switch function for ranks

--- src/components/guild-roster.js
+++ src/components/guild-roster.js
@@ -1,18 +1,19 @@
 import React, { Component } from 'react';
 
 class GuildRoster extends Component {
+// MAC: Define base state and pass through variables needed for fetch
     constructor(props) {
         super(props);
         this.state = {
             error: null,
             isLoaded: false,
             character: [],
-            wowClass: [],
+            // wowClass: [],
             dataRoute: 'https://us.api.battle.net/wow/guild/illidan/HAX?fields=members&locale=en_US&apikey=gyu8rq8enunpykunew34bmnnubbb6qah',
-            classRoute: 'https://us.api.battle.net/wow/data/character/classes?locale=en_US&apikey=gyu8rq8enunpykunew34bmnnubbb6qah'
+            // classRoute: 'https://us.api.battle.net/wow/data/character/classes?locale=en_US&apikey=gyu8rq8enunpykunew34bmnnubbb6qah'
         };
     }
-
+// MAC: Mount the dataroute from constructor, fetch the data then populate the character array
     componentDidMount() {
         fetch(this.state.dataRoute).then(res => res.json())
             .then(
@@ -29,41 +30,45 @@ class GuildRoster extends Component {
                     });
                 }
             )
-        .then( fetch(this.state.classRoute).then(res => res.json())
-            .then(
-                (result) => {
-                    this.setState({
-                        isLoaded: true,
-                        wowClass: result.classes
-                    });
-                },
-            (error) => {
-                this.setState({
-                    isLoaded: true,
-                    error
-                })
-            }
-          )
-        )
+// MAC: 2nd fetch that grabs classID and Name. Not useful right now until switches are removed.
+        // .then( fetch(this.state.classRoute).then(res => res.json())
+        //     .then(
+        //         (result) => {
+        //             this.setState({
+        //                 isLoaded: true,
+        //                 wowClass: result.classes
+        //             });
+        //         },
+        //     (error) => {
+        //         this.setState({
+        //             isLoaded: true,
+        //             error
+        //         })
+        //     }
+        //   )
+        // )
                
     }
 
     render() {
-        const { error, isLoaded, character, wowClass} = this.state;
+// MAC: Grab values of error, isLoaded and character for mapping and if statement
+        const { error, isLoaded, character, /*wowClass*/} = this.state;
         
         if (error) {
             return <div>Error: {error.message}</div>;
         } else if (!isLoaded){
             return <div>Loading...</div>
         } else {
+// MAC: Build the page
             console.log(this.state);
             return [
                 <div className="row">
                     <div className="column c12"><h1>Guild Roster</h1>
                 </div>
-                {wowClass.map((c, i) => 
+{/* MAC: Map function for showing class ID and Name from 2nd fetch.*/}
+                {/* wowClass.map((c, i) => 
                     <ul key={c.id} className="column c12"><li>id: {c.id} Name: {c.name}</li></ul>
-                )}
+                )*/}
                 {character.map((char, i) => 
                 <ul key={i} className="column c2">
                         <li key={char.character.thumbnail}>
@@ -72,10 +77,10 @@ class GuildRoster extends Component {
                                 alt="character portrait"
                             />
                         </li>
-                        <li><h3 key={char.character.name}><a href={`https://worldofwarcraft.com/en-us/character/${char.character.realm}/${char.character.name}`} title={`WoW armoury${char.character.name}`}>{char.character.name}</a></h3></li>
-                        <li key={char.character.level}>Level: {char.character.level}</li>
-                        <li key={char.character.acheivementPoints}>Class: {getClassName(char.character.class)}</li>
-                        <li key={char.rank}>Rank: {char.rank}</li>
+                        <li><h3 key={char.character.name}><a href={`https://worldofwarcraft.com/en-us/character/${char.character.realm}/${char.character.name}`} title={`Armoury for ${char.character.name}`}>{char.character.name}</a></h3></li>
+                        <li key={char.character.level}><b>Level:</b> {char.character.level}</li>
+                        <li key={char.character.acheivementPoints}><b>Class:</b> {getClassName(char.character.class)}</li>
+                        <li key={char.rank}><b>Rank:</b> {getRankName(char.rank)}</li>
                 </ul>
                 )}
                 </div>
@@ -130,6 +135,35 @@ function getClassName(classID) {
     }        
 
     return className;
+}
+// MAC: Returns a rank Name from the provided rankID
+function getRankName(rankID) {
+    var rankName = "";
+
+    switch(rankID) {
+        case 0:
+            rankName = 'Guild Master'
+            break;
+        case 1:
+            rankName = 'Officer'
+            break;
+        case 2:
+            rankName = 'Recruit Officer'
+            break;
+        case 3:
+            rankName = 'Raiders'
+            break;
+        case 4:
+            rankName = 'Members'
+            break;
+        case 5: 
+            rankName = 'Initiate'
+            break;
+        default: 
+            rankName = 'Unknown'
+    }
+
+    return rankName;
 }
 
 export default GuildRoster;
